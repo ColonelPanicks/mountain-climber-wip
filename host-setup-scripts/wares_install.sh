afe7b3cbf852de938aebe95e3b478173b03a0c9f
stuts <stu.j.franks@gmail.com>
2018-12-11 17:35:14 +0000

    Modular installer and bump versions

--- host-setup-scripts/wares_install.sh
+++ host-setup-scripts/wares_install.sh
@@ -2,82 +2,130 @@
 
 DATE=$(date +%Y%m%d)
 
-UNDERWARE_VERSION=0.3.0
+UNDERWARE_VERSION=0.4.0
 METALWARE_VERSION=develop
-CLOUDWARE_VERSION=0.1.0.pre3
+CLOUDWARE_VERSION=0.1.0.pre4
 ADMINWARE_VERSION=2018.2.0
-USERWARE_VERSION=feature/simple-group-commands
+USERWARE_VERSION=feature/advanced-mode
 
+#
+# Functions
+#
 
-# Tidy up
-echo "Checking for existing installations..."
-for i in underware metalware cloudware adminware; do 
-  [ -d /opt/$i ] && mv /opt/$i /opt/$DATE-$i
-  [ -d /var/lib/$i ] && mv /var/lib/$i /var/lib/$DATE-$i
-done
+function tidy_up() {
+  WARE=$1
+  [ -d /opt/$WARE ] && mv /opt/$WARE /opt/$DATE-$WARE
+  [ -d /var/lib/$WARE ] && mv /var/lib/$WARE /var/lib/$DATE-$WARE
+}
 
+function install_underware() {
+  curl -sL https://raw.githubusercontent.com/alces-software/underware/master/scripts/bootstrap?installer/ | alces_OS=el7 alces_SOURCE_BRANCH=$UNDERWARE_VERSION /bin/bash
+  mkdir -p /var/lib/underware/repo/{config,genders}
 
-# Underware
-curl -sL https://raw.githubusercontent.com/alces-software/underware/master/scripts/bootstrap?installer/ | alces_OS=el7 alces_SOURCE_BRANCH=$UNDERWARE_VERSION /bin/bash
-mkdir -p /var/lib/underware/repo/{.git,config,genders}
-curl https://raw.githubusercontent.com/ColonelPanicks/mountain-climber-wip/master/underware/repo-to-be-included/config/domain.yaml > /var/lib/underware/repo/config/domain.yaml
-curl https://raw.githubusercontent.com/ColonelPanicks/mountain-climber-wip/master/underware/repo-to-be-included/configure.yaml > /var/lib/underware/repo/configure.yaml
-curl https://raw.githubusercontent.com/ColonelPanicks/mountain-climber-wip/master/underware/data/templates/genders > /var/lib/underware/repo/genders/default
+  if cd /root/mountain-climber-wip ; then
+    git pull
+  else
+    git clone https://github.com/ColonelPanicks/mountain-climber-wip/ /root/mountain-climber-wip/
+  fi
+}
 
-# Metalware
-curl -sL https://raw.githubusercontent.com/alces-software/metalware/master/scripts/bootstrap?installer | alces_OS=el7 alces_SOURCE_BRANCH=$METALWARE_VERSION /bin/bash
+function install_metalware() {
+  curl -sL https://raw.githubusercontent.com/alces-software/metalware/master/scripts/bootstrap?installer | alces_OS=el7 alces_SOURCE_BRANCH=$METALWARE_VERSION /bin/bash
 
-# Cloudware
-curl -sL https://raw.githubusercontent.com/alces-software/cloudware/master/scripts/bootstrap | alces_OS=el7 alces_SOURCE_BRANCH=$CLOUDWARE_VERSION /bin/bash
+}
 
-# Adminware
-cd /opt
-git clone https://github.com/alces-software/adminware.git
-cd adminware
-git checkout $ADMINWARE_VERSION
-make setup
-cd /opt/adminware/bin
-curl https://s3-eu-west-1.amazonaws.com/flightconnector/adminware/resources/sandbox-starter > sandbox-starter
+function install_cloudware() {
+  curl -sL https://raw.githubusercontent.com/alces-software/cloudware/master/scripts/bootstrap | alces_OS=el7 alces_SOURCE_BRANCH=$CLOUDWARE_VERSION /bin/bash
 
-# Userware
-echo "checking for existing userware"
-[ -d /opt/directory ] && mv /opt/directory /opt/$DATE-directory
-[ -d /opt/share ] && mv /opt/share /opt/$DATE-share
-[ -d /tmp/userware ] && rm -rf /tmp/userware
+}
 
-git clone https://github.com/alces-software/userware /tmp/userware
-cd /tmp/userware
-git checkout $USERWARE_VERSION
-rsync -auv /tmp/userware/{directory,share} /opt/
-cd /opt/directory/cli
-make setup
-mkdir /opt/directory/etc/
-echo "cw_ACCESS_fqdn=$(hostname -f)" > /opt/directory/etc/access.rc
-mkdir -p /var/www/html/secure
-cd /opt/directory/cli/bin
-# Download resources/sandbox-starter from this repo.
-curl https://s3-eu-west-1.amazonaws.com/flightconnector/directory/resources/sandbox-starter > sandbox-starter
+function install_adminware() {
+  cd /opt
+  git clone https://github.com/alces-software/adminware.git
+  cd adminware
+  git checkout $ADMINWARE_VERSION
+  cd /opt/adminware/bin
+  curl https://s3-eu-west-1.amazonaws.com/flightconnector/adminware/resources/sandbox-starter > sandbox-starter
 
+  install_branding
 
-# Install branding for userware/adminware
-mkdir -p /opt/flight/bin
-cd /opt/flight/bin
-# Download resources/banner from this repo.
-curl https://s3-eu-west-1.amazonaws.com/flightconnector/directory/resources/banner > banner
-chmod 755 banner
-
-# Finishing messages
-cat << EOF
+  cat << EOF
 ===============================================================
 
-The installation of all of the wares is complete.
+There are still some additional things that require setting
+up in order to get full functionality:
+
+- Creation of clusteradmin user for adminware sandbox
+
+===============================================================
+EOF
+}
+
+function install_userware() {
+  echo "checking for existing userware"
+  [ -d /opt/directory ] && mv /opt/directory /opt/$DATE-directory
+  [ -d /opt/share ] && mv /opt/share /opt/$DATE-share
+  [ -d /tmp/userware ] && rm -rf /tmp/userware
+
+  git clone https://github.com/alces-software/userware /tmp/userware
+  cd /tmp/userware
+  git checkout $USERWARE_VERSION
+  rsync -auv /tmp/userware/{directory,share} /opt/
+  cd /opt/directory/cli
+  make setup
+  echo "cw_ACCESS_fqdn=$(hostname -f)" > /opt/directory/etc/access.rc
+  mkdir -p /var/www/html/secure
+
+  install_branding
+
+  cat << EOF
+===============================================================
 
 There are still some additional things that require setting
 up in order to get full functionality:
 
 - IPAPASSWORD=MyIPApassHere into /opt/directory/etc/config
-- Creation of clusteradmin user for adminware sandbox
 - Creation of useradmin user for userware sandbox
 
 ===============================================================
 EOF
+}
+
+function install_branding() {
+  mkdir -p /opt/flight/bin
+  cd /opt/flight/bin
+  # Download resources/banner from this repo.
+  curl https://s3-eu-west-1.amazonaws.com/flightconnector/directory/resources/banner > banner
+  chmod 755 banner
+}
+
+function tidy_and_install() {
+  WARE=$1
+  echo "Installing $WARE"
+  if [[ $WARE != "userware" ]] ; then
+    tidy_up $WARE
+  fi
+  install_$WARE
+}
+
+#
+# Run it
+#
+
+INSTALL=$1
+
+case $INSTALL in
+  "underware"|"metalware"|"cloudware"|"adminware"|"userware")
+  tidy_and_install $INSTALL
+  ;;
+  "")
+  echo "Installing Underware, Metalware, Cloudware, Adminware and Userware"
+  for i in underware metalware cloudware adminware userware ; do
+    tidy_and_install $INSTALL
+  done
+  ;;
+  *)
+  echo "Invalid argument, installing nothing"
+  ;;
+esac
+
