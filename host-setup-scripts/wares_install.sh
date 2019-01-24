c0ac14607bd3361549fde3b103f7b5246d64097f
stuts <stu.j.franks@gmail.com>
2019-01-24 13:12:42 +0000

    Add inventoryware, fix indents

--- host-setup-scripts/wares_install.sh
+++ host-setup-scripts/wares_install.sh
@@ -2,44 +2,59 @@
 
 DATE=$(date +%Y%m%d)
 
-UNDERWARE_VERSION="${UNDERWARE_VERSION:-0.4.0}"
+UNDERWARE_VERSION="${UNDERWARE_VERSION:-0.6.0-rc1}"
 METALWARE_VERSION="${METALWARE_VERSION:-develop}"
 CLOUDWARE_VERSION="${CLOUDWARE_VERSION:-2019.1.0.rc2}"
 ADMINWARE_VERSION="${ADMINWARE_VERSION:-2018.2.0}"
 USERWARE_VERSION="${USERWARE_VERSION:-feature/advanced-mode}"
+INVENTORYWARE_VERSION="${INVENTORYWARE_VERSION:-2019.1.0}"
 
 #
 # Functions
 #
 
 function tidy_up() {
-  WARE=$1
-  [ -d /opt/$WARE ] && mv /opt/$WARE /opt/$DATE-$WARE
-  [ -d /var/lib/$WARE ] && mv /var/lib/$WARE /var/lib/$DATE-$WARE
+    WARE=$1
+    [ -d /opt/flight/tools/$WARE ] && mv /opt/flight/tools/$WARE /opt/flight/tools/$DATE-$WARE
+    [ -d /var/lib/$WARE ] && mv /var/lib/$WARE /var/lib/$DATE-$WARE
+}
+
+function install_flightcore() {
+    if [ -x /opt/flight/bin/flight ] ; then
+        yum install https://s3-eu-west-1.amazonaws.com/alces-flight/rpms/flight-core-0.1.0%2B20190121150201-1.el7.x86_64.rpm
+    fi
+    source /etc/profile.d/alces-flight.sh
 }
 
 function install_underware() {
-  curl -sL https://raw.githubusercontent.com/alces-software/underware/master/scripts/bootstrap?installer/ | alces_OS=el7 alces_SOURCE_BRANCH=$UNDERWARE_VERSION /bin/bash
-  mkdir -p /var/lib/underware/repo/{config,genders}
+    install_flightcore
+    curl https://raw.githubusercontent.com/alces-software/underware/master/scripts/install |alces_VERSION=$UNDERWARE_VERSION /bin/bash
+    source /etc/profile.d/alces-flight.sh
 
-  if cd /root/mountain-climber-wip ; then
-    git pull
-  else
-    git clone https://github.com/ColonelPanicks/mountain-climber-wip/ /root/mountain-climber-wip/
-  fi
+    if cd /root/mountain-climber-wip ; then
+        git pull
+    else
+        git clone https://github.com/ColonelPanicks/mountain-climber-wip/ /root/mountain-climber-wip/
+    fi
+}
+
+function install_inventoryware() {
+    install_flightcore
+    curl https://raw.githubusercontent.com/alces-software/inventoryware/master/scripts/install |alces_VERSION=$INVENTORYWARE_VERSION /bin/bash
+    source /etc/profile.d/alces-flight.sh
 }
 
 function install_metalware() {
-  curl -sL https://raw.githubusercontent.com/alces-software/metalware/master/scripts/bootstrap?installer | alces_OS=el7 alces_SOURCE_BRANCH=$METALWARE_VERSION /bin/bash
+    curl -sL https://raw.githubusercontent.com/alces-software/metalware/master/scripts/bootstrap?installer | alces_OS=el7 alces_SOURCE_BRANCH=$METALWARE_VERSION /bin/bash
 
 }
 
 function install_cloudware() {
-  cd /opt/
-  git clone https://github.com/alces-software/cloudware
-  cd cloudware
-  git checkout $CLOUDWARE_VERSION
-  cat << EOF
+    cd /opt/
+    git clone https://github.com/alces-software/cloudware
+    cd cloudware
+    git checkout $CLOUDWARE_VERSION
+    cat << EOF
 ===============================================================
 
 There are still some additional things that require setting
@@ -58,16 +73,16 @@ EOF
 }
 
 function install_adminware() {
-  cd /opt
-  git clone https://github.com/alces-software/adminware.git
-  cd adminware
-  git checkout $ADMINWARE_VERSION
-  cd /opt/adminware/bin
-  curl https://s3-eu-west-1.amazonaws.com/flightconnector/adminware/resources/sandbox-starter > sandbox-starter
+    cd /opt
+    git clone https://github.com/alces-software/adminware.git
+    cd adminware
+    git checkout $ADMINWARE_VERSION
+    cd /opt/adminware/bin
+    curl https://s3-eu-west-1.amazonaws.com/flightconnector/adminware/resources/sandbox-starter > sandbox-starter
 
-  install_branding
+    install_branding
 
-  cat << EOF
+    cat << EOF
 ===============================================================
 
 There are still some additional things that require setting
@@ -80,23 +95,23 @@ EOF
 }
 
 function install_userware() {
-  echo "checking for existing userware"
-  [ -d /opt/directory ] && mv /opt/directory /opt/$DATE-directory
-  [ -d /opt/share ] && mv /opt/share /opt/$DATE-share
-  [ -d /tmp/userware ] && rm -rf /tmp/userware
+    echo "checking for existing userware"
+    [ -d /opt/directory ] && mv /opt/directory /opt/$DATE-directory
+    [ -d /opt/share ] && mv /opt/share /opt/$DATE-share
+    [ -d /tmp/userware ] && rm -rf /tmp/userware
 
-  git clone https://github.com/alces-software/userware /tmp/userware
-  cd /tmp/userware
-  git checkout $USERWARE_VERSION
-  rsync -auv /tmp/userware/{directory,share} /opt/
-  cd /opt/directory/cli
-  make setup
-  echo "cw_ACCESS_fqdn=$(hostname -f)" > /opt/directory/etc/access.rc
-  mkdir -p /var/www/html/secure
+    git clone https://github.com/alces-software/userware /tmp/userware
+    cd /tmp/userware
+    git checkout $USERWARE_VERSION
+    rsync -auv /tmp/userware/{directory,share} /opt/
+    cd /opt/directory/cli
+    make setup
+    echo "cw_ACCESS_fqdn=$(hostname -f)" > /opt/directory/etc/access.rc
+    mkdir -p /var/www/html/secure
 
-  install_branding
+    install_branding
 
-  cat << EOF
+    cat << EOF
 ===============================================================
 
 There are still some additional things that require setting
@@ -110,20 +125,20 @@ EOF
 }
 
 function install_branding() {
-  mkdir -p /opt/flight/bin
-  cd /opt/flight/bin
-  # Download resources/banner from this repo.
-  curl https://s3-eu-west-1.amazonaws.com/flightconnector/directory/resources/banner > banner
-  chmod 755 banner
+    mkdir -p /opt/flight/bin
+    cd /opt/flight/bin
+    # Download resources/banner from this repo.
+    curl https://s3-eu-west-1.amazonaws.com/flightconnector/directory/resources/banner > banner
+    chmod 755 banner
 }
 
 function tidy_and_install() {
-  WARE=$1
-  echo "Installing $WARE"
-  if [[ $WARE != "userware" ]] ; then
-    tidy_up $WARE
-  fi
-  install_$WARE
+    WARE=$1
+    echo "Installing $WARE"
+    if [[ $WARE != "userware" ]] ; then
+        tidy_up $WARE
+    fi
+    install_$WARE
 }
 
 #
@@ -133,17 +148,17 @@ function tidy_and_install() {
 INSTALL=$1
 
 case $INSTALL in
-  "underware"|"metalware"|"cloudware"|"adminware"|"userware")
-  tidy_and_install $INSTALL
-  ;;
-  "")
-  echo "Installing Underware, Metalware, Cloudware, Adminware and Userware"
-  for i in underware metalware cloudware adminware userware ; do
-    tidy_and_install $INSTALL
-  done
-  ;;
-  *)
-  echo "Invalid argument, installing nothing"
-  ;;
+    "underware"|"metalware"|"cloudware"|"adminware"|"userware"|"inventoryware")
+        tidy_and_install $INSTALL
+        ;;
+    "")
+        echo "Installing Underware, Metalware, Cloudware, Adminware, Userware and Inventoryware"
+        for i in underware metalware cloudware adminware userware inventoryware ; do
+            tidy_and_install $INSTALL
+        done
+        ;;
+    *)
+        echo "Invalid argument, installing nothing"
+        ;;
 esac
 
